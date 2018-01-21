8a7e90e7f0179b0f8538717e031b6b215ac95eaf
Matt Hunt <mstephenhunt@gmail.com>
2018-01-21 16:33:52 -0500

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(_express2.default.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
-  response.send('Hello World!');
+  response.send('Hello World -- Production!');
 });
 
 app.listen(app.get('port'), function () {
