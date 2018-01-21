6a97f0fe6862bf142c77b67710ebee9fbd34d754
Matt Hunt <mstephenhunt@gmail.com>
2018-01-21 16:39:55 -0500

    auto build, add, commit, and push scripts

--- scripts/prod-push.sh
+++ scripts/prod-push.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+# ensures you're on master
+git checkout development
+
+# builds
+npm run build
+
+# auto add and commit
+git add dist
+git commit -m "auto-build"
+
+# pushes to git
+git push
+
+# pushes to heroku
 git push production master:master
