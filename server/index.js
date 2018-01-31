a8a87527b6a95aa9453134031bd3bab57e5c11c6
Matt Hunt <mstephenhunt@gmail.com>
2018-01-31 10:49:35 -0500

    serving static client assets

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname+'/../client/build/index.html'));
+  response.sendFile(path.join(__dirname+'/../../client/build/index.html'));
 })
 
 app.get('/users', function (request, response, next) {
