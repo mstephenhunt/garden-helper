68e0dcdadeb0a6e733e3915c2b9e2d3bdba422b5
Matt Hunt <mstephenhunt@gmail.com>
2018-02-04 06:05:57 -0500

    posts to Login endpoint

--- client/src/login.js
+++ client/src/login.js
@@ -17,9 +17,19 @@ export default class Login extends Component {
   }
 
   handleSubmit (event) {
-    const { username, password } this.state
+    const { username, password } = this.state
 
-    console.log(this.state)
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+       'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
 
     event.preventDefault()
   }
